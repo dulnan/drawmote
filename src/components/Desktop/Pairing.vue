e2d656ec8084cf4290da81eb3ba264f85ff7f2d1
Jan Hug <me@dulnan.net>
2018-09-12 09:28:05 +0200

    Add logo and favicons

--- src/components/Desktop/Pairing.vue
+++ src/components/Desktop/Pairing.vue
@@ -1,39 +1,40 @@
 <template>
   <div class="overlay pairing relative flex pdg++">
-    <div class="flex flex--column">
-      <h1 class="text-heavy mrgb+">drawmote</h1>
-      <p class="text-muted text-light mrgt0 h2 pairing-lead">{{ $t('desktop.lead') }}</p>
-      <h2 class="code mrgv++">
-        <transition name="appear">
-          <div v-if="code" class="code__content">
-            <div
-              class="code-circle contains"
-              :class="'code-circle--' + number"
-              v-for="(number, index) in pairingCodeNumbers"
-              :key="index"
-            >
-              <span>{{ number }}</span>
-            </div>
+    <logo />
+    <h1 class="text-heavy mrgb+ mrgt+">drawmote</h1>
+    <p class="text-muted text-light mrgt0 h2 pairing-lead">{{ $t('desktop.lead') }}</p>
+    <h2 class="code mrgv++">
+      <transition name="appear">
+        <div v-if="code" class="code__content">
+          <div
+            class="code-circle contains"
+            :class="'code-circle--' + number"
+            v-for="(number, index) in pairingCodeNumbers"
+            :key="index"
+          >
+            <span>{{ number }}</span>
           </div>
-        </transition>
-      </h2>
-      <div class="actions mrgt">
-        <button @click.prevent="skipPairing" type="button" class="btn btn--bare btn--small">{{ $t('desktop.skipButton') }}</button>
-      </div>
-      <browser-support checks="desktop" />
+        </div>
+      </transition>
+    </h2>
+    <div class="actions mrgt">
+      <button @click.prevent="skipPairing" type="button" class="btn btn--bare btn--small">{{ $t('desktop.skipButton') }}</button>
     </div>
+    <!-- <browser-support checks="desktop" /> -->
   </div>
 </template>
 
 <script>
 import { EventBus } from '@/events'
 
+import Logo from '@/components/Logo.vue'
 import BrowserSupport from '@/components/BrowserSupport.vue'
 
 export default {
   name: 'Pairing',
 
   components: {
+    Logo,
     BrowserSupport
   },
 
@@ -68,6 +69,10 @@ export default {
 .pairing {
   transform-style: preserve-3d;
   perspective: 700px;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
   &.appear-enter-active, &.appear-leave-active {
     transition: .5s;
     .pairing__content {
@@ -100,11 +105,10 @@ export default {
 .pairing-lead {
   max-width: 34rem;
   @include media('lg') {
-    max-width: 44rem;
+    max-width: 40rem;
   }
 }
 
 .actions {
-  margin-bottom: auto;
 }
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="logo">
+    <logo-stroke />
+  </div>
+</template>
+
+<script>
+import LogoStroke from '@/assets/images/logo.svg'
+
+export default {
+  name: 'Logo',
+
+  components: {
+    LogoStroke
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss">
+$logo-base: 768;
+
+.logo {
+  position: relative;
+  font-size: 15rem;
+  width: 1em;
+  height: 1em;
+  background: white;
+  border-radius: 0.2em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: radial-gradient(46% -46%, #FFFFFF 52%, #EAEAEA 100%);
+  background-image: linear-gradient(21deg, #FFFFFF 14%, rgba(255,255,255,0.00) 89%);
+  box-shadow: 0 ((14 / $logo-base) * 1em) ((90 / $logo-base) * 1em) rgba(0,0,0,0.06);
+
+  svg {
+    height: auto;
+    width: 70%;
+    display: block;
+  }
+
+  &:before {
+    content: "";
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    opacity: 0.8;
+    background: radial-gradient(
+      circle at 50% 35%,
+      #F2F1EF   0%,
+
+      white    10%,
+
+      #EAE8E5  20%,
+      #EDECE9  20%,
+
+      #E6E3DF  30%,
+      #EAE8E5  30%,
+
+      #E3DFDA  40%,
+      #E6E3DF  40%,
+
+      #DFDBD5  50%,
+      #E3DFDA  50%,
+
+      #DCD7D0  60%,
+      #DFDBD5  60%,
+
+      #D8D2CA  70%,
+      #DCD7D0  70%,
+
+      #D8D2CA  80%
+      );
+    z-index: -1;
+  }
+}
+</style>
