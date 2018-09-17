2cc140078f4eb8cdf11821de52bd5af305c239a7
Jan Hug <me@dulnan.net>
2018-09-17 18:44:18 +0200

    include matomo directly

--- src/main.js
+++ src/main.js
@@ -9,14 +9,7 @@ import DataHandler from './tools/DataHandler'
 import './assets/scss/main.scss'
 
 import VueLoop from './plugins/VueLoop'
-import VueMatomo from 'vue-matomo'
 import i18n from './i18n'
-
-Vue.use(VueMatomo, {
-  host: 'https://stats.dulnan.net',
-  siteId: 6,
-  trackInitialView: true
-})
 
 Vue.use(VueLoop)
 
@@ -28,9 +21,6 @@ Vue.prototype.$global = global
 Vue.prototype.$connection = new Connection(EventBus, global)
 
 Vue.prototype.$track = function (category, action, value) {
-  if (!window._paq) {
-    window._paq = []
-  }
   window._paq.push(['trackEvent', category, action, value])
 }
 
