2717ed1a424b5191f78f4b05a5e8f27950f544d2
Jan Hug <me@dulnan.net>
2019-05-30 08:57:31 +0200

    Track version numbers of important packages in sentry

--- src/plugins/Sentry.js
+++ src/plugins/Sentry.js
@@ -1,6 +1,8 @@
 import * as Sentry from '@sentry/browser'
 import * as Integrations from '@sentry/integrations'
 
+import dependencies from '@/tools/dependencies'
+
 export default {
   install(Vue) {
     const handler = {
@@ -15,6 +17,12 @@ export default {
         release: `drawmote@${process.env.PKG_VERSION}`,
         environment: process.env.VUE_APP_SERVER_ENV,
         integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+      })
+
+      Sentry.configureScope(scope => {
+        Object.keys(dependencies).forEach(key => {
+          scope.setTag('library_' + key, dependencies[key])
+        })
       })
 
       handler.setUser = function (id) {
---
+++ src/tools/dependencies.js
@@ -0,0 +1,5 @@
+// DEPENDENCIES is defined in vue.config.js as a webpack plugin and contains
+// version numbers for a few main dependencies of drawmote.
+
+// eslint-disable-next-line no-undef
+export default DEPENDENCIES
